031d5dd2d249e95a3205ebbbd81c2d247156d34a
h-suzuki <h-suzuki@royalgate.co.jp>
2014-10-25 21:28:25 +0900

    p.69まで

--- module/Application/src/Application/Controller/UserController.php
+++ module/Application/src/Application/Controller/UserController.php
@@ -9,6 +9,11 @@ class UserController extends AbstractActionController
 {
     public function addAction()
     {
-        return new ViewModel();
+        // ビューへ渡す値を配列にて定義
+        $values = array(
+            'key1' => 'value1',
+            'key2' => 'value2',
+        );
+        return new ViewModel($values);
     }
 }
