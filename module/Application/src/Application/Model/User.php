08d38d8e5bb8c392f97e1fc01147bfa183d53d90
h-suzuki <h-suzuki@royalgate.co.jp>
2014-10-25 22:49:41 +0900

    p.87まで

---
+++ module/Application/src/Application/Model/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Model;
+
+class User
+{
+    public $id;
+    public $name;
+    public $email;
+}
+    public function exchangeArray($data)
+    {
+        $this->id             = (isset($data['id'])) ? $data['id'] : 0;
+        $this->name           = (isset($data['name'])) ? $data['name'] : '';
+        $this->email          = (isset($data['email'])) ? $data['email'] : '';
+    }
+}
+
---
+++ module/Application/src/Application/Model/UserTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Application\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class UserTable{
+    protected $tableGateway;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function fetchAll()
+    {
+        $resultSet = $this->tableGateway->select();
+        return $resultSet;
+    }
+
+    public function getUser($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+    }
+
+    public function saveUser(User $user)
+    {
+        $data = array(
+            'name'      => $user->name,
+            'email'     => $user->email,
+        );
+        $id = (int)$user->id;
+        if ($id == 0) {
+            $this->tableGateway->insert($data);
+        } else {
+            if ($this->getUser($id)) {
+                $this->tableGateway->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Form id does not exist');
+            }
+        }
+    }
+}
